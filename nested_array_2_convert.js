b480c15d2a215a966e03deecb794badc3c8982ed
rachmadza <razato.zai@gmail.com>
2018-11-28 16:56:40 +0700

    push anchor nested array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -13,9 +13,33 @@ let roster = [
 
 function convert_roster_format (nestedArray) {
   // your convert code here
+  let output = []
+
+  for (let i = 1; i < nestedArray.length; i++) {
+    let obj ={}
+    for (let j = 0; j < nestedArray[i].length; j++) {
+      obj[nestedArray[0][j]] = nestedArray[i][j]
+    }
+
+    // obj[nestedArray[0][0]] = nestedArray[i][0]
+    // obj[nestedArray[0][1]] = nestedArray[i][1]
+    // obj[nestedArray[0][2]] = nestedArray[i][2]
+    // obj[nestedArray[0][3]] = nestedArray[i][3]
+
+    
+    output.push(obj)
+
+  }
+
+  return output
 }
 
+// console.log(convert_roster_format(roster));
+
+
+
 let object_roster = convert_roster_format(roster)
+
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
