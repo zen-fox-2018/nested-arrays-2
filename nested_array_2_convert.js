dcde6aa0c3a36de3b18cd1024d5fd1b54a319308
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 12:26:02 +0700

    fauzan

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -14,11 +14,9 @@ let roster = [
 function convert_roster_format (nestedArray) {
   let result = []
   for (let i = 1; i < nestedArray.length; i++) {
-    let obj = {
-      [nestedArray[0][0]]: nestedArray[i][0],
-      [nestedArray[0][1]]: nestedArray[i][1],
-      [nestedArray[0][2]]: nestedArray[i][2],
-      [nestedArray[0][3]]: nestedArray[i][3]
+    let obj = {}
+    for (let j = 0; j < nestedArray[0].length; j++) {
+      obj[nestedArray[0][j]] = nestedArray[i][j]
     }
     result.push(obj)
   }
